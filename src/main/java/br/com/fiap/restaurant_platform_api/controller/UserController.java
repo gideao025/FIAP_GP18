731a9b7d63628fd852af30bc2fa4555083e492e0
Victor <victormazzola2019@gmail.com>
2026-03-18 19:05:39 -0300

    feat: add user controller with create and getById endpoints

---
+++ src/main/java/br/com/fiap/restaurant_platform_api/controller/UserController.java
@@ -0,0 +1,31 @@
+package br.com.fiap.restaurant_platform_api.controller;
+
+import br.com.fiap.restaurant_platform_api.entity.User;
+import br.com.fiap.restaurant_platform_api.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user); //todo change later
+    }
+}
