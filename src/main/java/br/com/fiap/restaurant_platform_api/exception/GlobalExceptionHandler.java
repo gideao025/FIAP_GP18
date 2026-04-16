a5ecbb7676987b1ddcbb85b0625bb4d6aab88b89
Victor <victormazzola2019@gmail.com>
2026-04-16 06:18:01 -0300

    feat: add global exception handler

---
+++ src/main/java/br/com/fiap/restaurant_platform_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package br.com.fiap.restaurant_platform_api.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleError(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
